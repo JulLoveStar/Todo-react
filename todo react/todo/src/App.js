c19d4023411190067a465d2907dbc9eaf98d6e4a
Macbook <jul.love.star@gmail.com>
2023-03-13 14:29:10 +0530

    add todo

---
+++ todo react/todo/src/App.js
@@ -0,0 +1,36 @@
+// import React, { useState } from 'react';
+// import './App.css';
+// import Header from './components/Header/Header';
+// import AddTodo from './components/AddTodo/AddTodo';
+// import TodoList from './components/todolist/TodoList';
+
+// function App() {
+
+//   const [todo, setTodo] = useState([
+//     {
+//       id:1,
+//       title: 'first',
+//       status: true
+//     },
+//     {
+//       id:2,
+//       title: 'second',
+//       status: true
+//     },
+//     {
+//       id:3,
+//       title: 'thirt',
+//       status: false
+//     },
+//   ])
+
+//   return (
+//     <div className="bg-pink-400">
+//       <Header todo={todo} setTodo={setTodo}/>
+//       <AddTodo todo={todo} setTodo={setTodo}/>
+//       <TodoList todo={todo} setTodo={setTodo}/>
+//     </div>
+//   );
+// }
+
+// export default App;
